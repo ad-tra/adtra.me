081b2bed21528b69c48390d95041170f06f4fd93
ad-tra <66345861+ad-tra@users.noreply.github.com>
2022-02-25 11:59:49 -0500

    fixed video-expl-blog broken link

--- src/pages/work.js
+++ src/pages/work.js
@@ -6,10 +6,9 @@ import Section from "../components/Section";
 
 export default function Work() {
     const projectsMap = [
-        {slug: "https://soflotutors.netlify.app/sat-march-2020", title: "SoFlo Tutors - Dynamic Gatsby Blog (WIP)"}, 
+        {slug: "https://soflotutors.netlify.app/2018-may-sat/reading/may-2018-section1-q3", title: "SoFlo Tutors - Dynamic Gatsby Blog (WIP)"}, 
+        { slug: "https://soflotutors.com/reg/consult",  title: "Registration Form with Async Data Validation" },
         { slug: "https://kashidajs.netlify.app/", title: "Kashida - Readability Enhancement Algorithm for the Arabic Script" },
-        // { slug: "https://www.figma.com/proto/Pp72uAcaRV2nQqCzrsz0kc/App-Design?node-id=34%3A34&scaling=scale-down&page-id=0%3A1&starting-point-node-id=34%3A34&hotspot-hints=0&hide-ui=1", title: "Missed Connections - cross platform social media app" },
-        { slug: "https://upbeat-aryabhata-049774.netlify.app/",  title: "Muberza - Data Analytics to Compare Tunisian Politicians " },
         { slug: "https://github.com/ad-tra/toth",  title: "Toth -  Educational Website for SAT students" },
         { slug: "https://github.com/ad-tra/school-skipper",   title: "School Skipper - Bot that attends school for you " },
     ]
